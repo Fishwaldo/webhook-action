47ebbc681ce526917a593502b133571e4c1aa60d
Justin Hammond <justin@dynam.ac>
2019-11-21 16:46:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-./bamboopr.rb --server $BBSERVER --user $BBUSER --pass $BBPASS -k $BBPRJ -r $BBPR
+/Bamboo-PR-Trigger/bamboopr.rb --server $BBSERVER --user $BBUSER --pass $BBPASS -k $BBPRJ -r $BBPR
 #curl -X PUT -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
